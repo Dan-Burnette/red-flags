d2c0444368dd26d11779babcc0569155b0b95425
Dan Burnette <dcburnette999@gmail.com>
2024-08-14 17:09:48 +0900

    Identify elements that *directly* contain the flag. I.e. they are not a parent of an element that contains the flag, but contain the flag themselves directly. Basically, get the lowest down the HTML tree node that contains the flag.

--- red-flags.js
+++ red-flags.js
@@ -19,3 +19,19 @@ console.log("detectedRedFlags", detectedRedFlags);
 console.log("detectedYellowFlags", detectedYellowFlags);
 
 // TODO: locate the element, append a red/yellow flag icon.
+// 1) Select INNERMOST? element containing the word.
+const pageElements = [ ...body.querySelectorAll("*") ];
+
+detectedRedFlags.forEach(flag => {
+  const elementsDirectlyContainingFlag = pageElements.filter(e => {
+    const containsFlag = e.textContent.includes(flag);
+    const childContainsFlag = [...e.children].some(child => child.textContent.includes(flag));
+    const directlyContainsFlag = containsFlag && ! childContainsFlag;
+    return directlyContainsFlag;
+  })
+});
+
+// 2) Replace content with the same stuff but added HTML
+// content...<p> class="red-flag">some red flag</p>...content
+//
+// 3) Add a red flag popup on top of 'red-flag' class elements
