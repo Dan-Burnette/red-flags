eb6769689b30007f7cb8cf29862720e5543f0db6
Dan Burnette <dcburnette999@gmail.com>
2024-08-14 16:26:23 +0900

    Detect presence of red/yellow flags on the page

--- red-flags.js
+++ red-flags.js
@@ -11,27 +11,11 @@ const yellowFlags = [
 ]
 
 const body = document.querySelector("body");
-
 const text = body.textContent;
-const wordMatchRegExp = /[^\s]+/g;
-const wordsIterator = text.matchAll(wordMatchRegExp);
-const wordsArray = [ ...wordsIterator ];
 
-// const wordCount = [...words].length;
-// const readingTime = Math.round(wordCount / 200);
-console.log("wordsArray", wordsArray);
-// TODO: detect red flags, locate the element, append a red flag icon.
+const detectedRedFlags = redFlags.filter(flag => text.includes(flag))
+const detectedYellowFlags = yellowFlags.filter(flag => text.includes(flag))
+console.log("detectedRedFlags", detectedRedFlags);
+console.log("detectedYellowFlags", detectedYellowFlags);
 
-// TODO: detect yellow flags, locate the element, append a yellow flag icon.
-
-const badge = document.createElement("p");
-// Use the same styling as the publish information in an article's header
-// badge.classList.add("color-secondary-text", "type--caption");
-// badge.textContent = `⏱️ ${readingTime} min read`;
-
-// Support for API reference docs
-// const heading = article.querySelector("h1");
-// Support for article docs with date
-// const date = article.querySelector("time")?.parentNode;
-
-// (date ?? heading).insertAdjacentElement("afterend", badge);
+// TODO: locate the element, append a red/yellow flag icon.
