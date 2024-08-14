d81209abe0c45fc640e6dd82fceaf8c6e10d3922
Dan Burnette <dcburnette999@gmail.com>
2024-08-14 17:53:27 +0900

    Display a red flag icon next to the red-flag-extension tagged elements via css's :after. It works

--- red-flags.js
+++ red-flags.js
@@ -15,14 +15,10 @@ const text = body.textContent;
 
 const detectedRedFlags = redFlags.filter(flag => text.includes(flag))
 const detectedYellowFlags = yellowFlags.filter(flag => text.includes(flag))
-console.log("detectedRedFlags", detectedRedFlags);
-console.log("detectedYellowFlags", detectedYellowFlags);
 
 const pageElements = [ ...body.querySelectorAll("*") ];
 
 detectedRedFlags.forEach(flag => {
-
-  // 1) Select element containing the word.
   const elementsDirectlyContainingFlag = pageElements.filter(e => {
     const containsFlag = e.textContent.includes(flag);
     const childContainsFlag = [...e.children].some(child => child.textContent.includes(flag));
@@ -30,14 +26,9 @@ detectedRedFlags.forEach(flag => {
     return directlyContainsFlag;
   })
 
-  // 2) Replace content with the same stuff but added HTML
-  // content...<p> class="red-flag">some red flag</p>...content
   elementsDirectlyContainingFlag.forEach(e => {
-    const newInnerHTML = e.innerHTML.replace(flag, `<span class='red-flag'>${flag}</span>`)
+    const newInnerHTML = e.innerHTML.replace(flag, `<span class='red-flag-extension'>${flag}</span>`)
     e.innerHTML = newInnerHTML;
   });
-
-
 });
 
-// 3) Add a red flag popup on top of 'red-flag' class elements
