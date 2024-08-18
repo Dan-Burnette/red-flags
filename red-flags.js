4b42f092b6802bb5ee1b57d32ca657f60149fe6b
Dan Burnette <dcburnette999@gmail.com>
2024-08-18 15:24:29 +0900

    add TODOS. Remove yellow flags. Red only

--- red-flags.js
+++ red-flags.js
@@ -2,33 +2,45 @@ const redFlags = [
   "hustle",
   "work hard play hard",
   "urgency",
-  "relentless"
+  "relentless",
+  "scrappy",
+  "fast-paced",
+  "fast paced"
 ]
 
-const yellowFlags = [
-  "shipping fast",
-  "scrappy"
-]
+// TODO: Make it work on linkedin and sites that load via XHR:
+//  it runs querySelectorAll on initial load, which will miss new content
+//  inserted into the DOM. 
+//  - Use MutationObserver to run on DOM insertions.
+//  - Run the function on the insertions, scanning for red flags
+// 
+// TODO: Make it case insensitive
+// TODO: specs
+window.addEventListener("load", (event) => {
+  const body = document.querySelector("body");
+  const text = body.textContent;
+  const detectedRedFlags = redFlags.filter(flag => text.includes(flag))
+  const pageElements = [ ...body.querySelectorAll("*") ];
 
-const body = document.querySelector("body");
-const text = body.textContent;
+  detectedRedFlags.forEach(flag => {
+    const elementsDirectlyContainingFlag = pageElements.filter(e => {
+      const containsFlag = e.textContent.includes(flag);
+      const childContainsFlag = [...e.children].some(child => child.textContent.includes(flag));
 
-const detectedRedFlags = redFlags.filter(flag => text.includes(flag))
-const detectedYellowFlags = yellowFlags.filter(flag => text.includes(flag))
+      if (containsFlag) {
+        console.log("e contains flag:", e);
+      }
+      const directlyContainsFlag = containsFlag && !childContainsFlag;
+      return directlyContainsFlag;
+    })
 
-const pageElements = [ ...body.querySelectorAll("*") ];
-
-detectedRedFlags.forEach(flag => {
-  const elementsDirectlyContainingFlag = pageElements.filter(e => {
-    const containsFlag = e.textContent.includes(flag);
-    const childContainsFlag = [...e.children].some(child => child.textContent.includes(flag));
-    const directlyContainsFlag = containsFlag && !childContainsFlag;
-    return directlyContainsFlag;
-  })
-
-  elementsDirectlyContainingFlag.forEach(e => {
-    const newInnerHTML = e.innerHTML.replace(flag, `<span class='red-flag-extension'>${flag}</span>`)
-    e.innerHTML = newInnerHTML;
+    console.log("elementsDirectlyContainingFlag", elementsDirectlyContainingFlag);
+    elementsDirectlyContainingFlag.forEach(e => {
+      const newInnerHTML = e.innerHTML.replace(flag, `<span class='red-flag-extension'>${flag}</span>`)
+      e.innerHTML = newInnerHTML;
+    });
   });
 });
+
+console.log("page elements", pageElements);
 
